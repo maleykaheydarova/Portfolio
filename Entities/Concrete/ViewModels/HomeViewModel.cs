bc8598e3f205a9e231d5e38cf3c3afa0fa9e6f57
maleykaheydarova <mheyderova11@gmail.com>
2024-01-22 23:36:35 +0400

    last

--- Entities/Concrete/ViewModels/HomeViewModel.cs
+++ Entities/Concrete/ViewModels/HomeViewModel.cs
@@ -9,10 +9,12 @@ namespace Entities.Concrete.ViewModels
 {
     public class HomeViewModel
     {
-        public  List<Service> Services { get; set; }
         public Person Person { get; set; }
         public List<Experience> Experiences { get; set; }
-        
+        public List<SkillDetails> SkillDetails { get; set; }
+        public List<Service> Services { get; set; }
+        public List<WorkCategory> WorkCategories { get; set; }
+        public List<Portfoli> Portfolio { get; set; }  
 
     }
 }
--- Portfolio/Controllers/HomeController.cs
+++ Portfolio/Controllers/HomeController.cs
@@ -9,19 +9,39 @@ namespace Portfolio.Controllers
     public class HomeController : Controller
     {
         public readonly IPersonService _personService;
+        public readonly IExperienceService _experienceService;
+        public readonly ISkillDetailsService _skillDetailsService;
+        public readonly IServiceService _serviceService;
+        public readonly IWorkCategoryService _workCategoryService;
+        public readonly IPortfolioService _portfolioService;
 
-        public HomeController(IPersonService personService)
+        public HomeController(IPersonService personService, IExperienceService experienceService, ISkillDetailsService skillDetailsService, IServiceService serviceService, IWorkCategoryService workCategoryService, IPortfolioService portfolioService)
         {
-           _personService= personService;
+            _personService = personService;
+            _experienceService = experienceService;
+            _skillDetailsService = skillDetailsService;
+            _serviceService = serviceService;
+            _workCategoryService = workCategoryService;
+            _portfolioService = portfolioService;
         }
 
         public IActionResult Index()
         {
             var personData = _personService.GetAll().Data[0];
+            var experienceData = _experienceService.GetAll().Data;
+            var skilldetailsData = _skillDetailsService.GetAll().Data;
+            var serviceData= _serviceService.GetAll().Data;
+            var workcategoryData= _workCategoryService.GetAll().Data;
+            var portfolioData= _portfolioService.GetAll().Data;
 
             HomeViewModel homeViewModel = new()
-            {
-                Person = personData
+            { 
+                Person = personData,
+                Experiences = experienceData,
+                SkillDetails = skilldetailsData,
+                Services=serviceData,
+                WorkCategories=workcategoryData,
+                Portfolio=portfolioData
             };
 
 
